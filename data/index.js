d8848c353adb752679a5586ac290775816dcf5e9
shashankmarch27 <shashankmarch27@gmail.com>
2023-11-06 21:40:28 +0530

    Fixed website
    Co-authored-by: sahil singh <sahilsingh2002@users.noreply.github.com>

--- data/index.js
+++ data/index.js
@@ -1,27 +1,43 @@
 console.log("Welcome MF");
+var checkbox = document.getElementById("myCheckbox");
 
-if (!!window.EventSource) {
+checkbox.addEventListener("change", function () {
+  if (this.querySelector("input").checked) {
+    fetchData();
+  }
+});
 
-var source = new EventSource('/events');
-source.addEventListener('current', function(e) {
-    console.log("current", e.data);
-    document.getElementById("current").innerHTML = e.data;
-   }, false);
-
-source.addEventListener('voltage', function(e) {
-    console.log("voltage", e.data);
-    document.getElementById("voltage").innerHTML = e.data;
-   }, false);
-
-source.addEventListener('power', function(e) {
-    console.log("power", e.data);
-    document.getElementById("power").innerHTML = e.data;
-   }, false);
-
-source.addEventListener('power_factor', function(e) {
+function fetchData() {
+    // function turnOn{
+    //     var valReq=new XMLHttpRequest();
+        
+    //     valReq.open("GET","/current",true);
+    //     valReq.send();
+    // }
+  // alert("Fetching data");
+  if (!!window.EventSource) {
+      
+      var source = new EventSource('/events');
+      source.addEventListener('current', function(e) {
+          console.log("current", e.data);
+          document.getElementById("current").innerHTML = e.data;
+        }, false);
+        
+        source.addEventListener('voltage', function(e) {
+            console.log("voltage", e.data);
+            document.getElementById("voltage").innerHTML = e.data;
+        }, false);
+        
+        source.addEventListener('power', function(e) {
+            console.log("power", e.data);
+            document.getElementById("power").innerHTML = e.data;
+        }, false);
+        
+        source.addEventListener('power_factor', function(e) {
     console.log("power_factor", e.data);
     document.getElementById("power_factor").innerHTML = e.data;
    }, false);
-
+   
+}
 }
 
